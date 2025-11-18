64e8cc5d5a37b0c165ffe0d7cba075ed25789d93
sDenisss <ddenis22072006@gmail.com>
2025-11-18 20:28:23 +0300

    added 463, 1544

---
+++ C#/1544. Make The String Great/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string a = "leEeetcode";
+        string b = "abBAcC";
+        string c = "";
+        System.Console.WriteLine(solution.MakeGood(a));
+        System.Console.WriteLine(solution.MakeGood(b));
+        System.Console.WriteLine(solution.MakeGood(c));
+    }
+}
+
+public class Solution {
+    public string MakeGood(string s) 
+    {
+        if (s == null || s.Length == 0)
+            return "";
+
+        Stack<char> stack = new Stack<char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (stack.Count > 0 && IsBadPair(stack.Peek(), s[i]))
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(s[i]);
+            }
+        }
+
+        char[] res = new char[stack.Count];
+        for (int i = stack.Count - 1; i >= 0; i--)
+        {
+            res[i] = stack.Pop();
+        }
+        return new string(res);
+    }
+
+    public bool IsBadPair(char c1, char c2)
+    {
+        if (c1.Equals(c2)) return false;
+        
+        return char.ToUpper(c1).Equals(c2) || char.ToLower(c1).Equals(c2) ? true : false;
+    }
+}
---
+++ C#/463. Island Perimeter/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[][] a = { [0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0] };
+        int[][] b = { [1] };
+        int[][] c = { [1, 0] };
+
+        // int[][] b = { 2,3,5 };
+
+
+        Solution sol = new Solution();
+        System.Console.WriteLine(sol.IslandPerimeter(a));
+        System.Console.WriteLine(sol.IslandPerimeter(b));
+        System.Console.WriteLine(sol.IslandPerimeter(c));
+    }
+}
+
+public class Solution
+{
+    public int IslandPerimeter(int[][] grid)
+    {
+        int perimeter = 0;
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    perimeter += 4;
+                    
+                    // Вычитаем за общие границы с соседями
+                    if (i > 0 && grid[i-1][j] == 1) perimeter -= 1;
+                    if (i < rows - 1 && grid[i+1][j] == 1) perimeter -= 1; 
+                    if (j > 0 && grid[i][j-1] == 1) perimeter -= 1; 
+                    if (j < cols - 1  && grid[i][j+1] == 1) perimeter -= 1; 
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+}
